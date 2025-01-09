5a27f7f3eed248e3d35f00738076baea74b9d3da
dmy.berezovskyi <dmy.berezovskyi@foodtech.team>
2025-01-09 18:12:43 +0200

    fix read docs location

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,5 +26,4 @@ highlight_language = "python"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
